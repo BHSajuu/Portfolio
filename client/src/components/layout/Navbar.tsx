8853777425de2ff6c7405eb1de5b9396c102a400
Boktiar Hussain <bokthairhussain@gmail.com>
2025-03-18 22:39:27 +0530

    resume update

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -52,7 +52,7 @@ export function Navbar() {
           <div className="flex items-center gap-4">
             <Button variant="outline" asChild>
               <a
-                href="https://drive.google.com/file/d/1v1owH2ekpQ718dfB4NqhGhEHXE_wpBEu/view?usp=sharing"
+                href="https://drive.google.com/file/d/1xsoWVvHzf4BsRGxmq8mi_9MZw3Zhth8N/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer">
                 Resume
