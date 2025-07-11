32519d57a92fc5d1acb5e1d9dedf714fbb3655f1
BHSajuu <boktiarh_ug_22@mech.nits.ac.in>
2025-07-12 00:07:26 +0530

    Cursor animation added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import NotFound from "@/pages/not-found";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Route, Switch } from "wouter";
 import { queryClient } from "./lib/queryClient";
+import SplashCursor from "./components/Animations/SplashCursor/SplashCursor";
 
 function Router() {
   return (
@@ -19,6 +20,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Router />
       <Toaster />
+      <SplashCursor/>
     </QueryClientProvider>
   );
 }
